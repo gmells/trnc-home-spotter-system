c28e3ea6551ce4b78d57dcdbf6c2623c20d06f39
gmells <victormells30@gmail.com>
2024-04-14 23:02:48 +0300

    created Google authenthication button

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function OAuth() {
+  return (
+    <button
+      type="button"
+      className="bg-red-600 text-white p-3 rounded-lg uppercase hover:opacity-80"
+    >
+      Log in with google
+    </button>
+  );
+}
